0f6fffca54222ed788f4b5d8505260548b2d295d
PtrMan <robertw89@googlemail.com>
2015-08-08 14:15:45 +0200

    fixed bug in DistinctUtility

--- src/main/java/ptrman/mltoolset/math/DistinctUtility.java
+++ src/main/java/ptrman/mltoolset/math/DistinctUtility.java
@@ -14,7 +14,7 @@ public class DistinctUtility {
         List<Integer> temporaryList = new ArrayList<>();
         temporaryList.add(randomNumber);
 
-        for( int counter = 0; counter < count; counter++ ) {
+        for( int counter = 0; counter < count-1; counter++ ) {
             List<Integer> additionalNumbers = getDisjuctNumbersTo(random, temporaryList, 1, max);
             temporaryList.addAll(additionalNumbers);
         }
