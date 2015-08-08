84eef1d4416e99264a2dab169f27cb00f657b508
PtrMan <robertw89@googlemail.com>
2015-08-08 14:07:53 +0200

    DistinctUtility refactoring

--- src/main/java/ptrman/mltoolset/math/DistinctUtility.java
+++ src/main/java/ptrman/mltoolset/math/DistinctUtility.java
@@ -6,39 +6,33 @@ import java.util.Random;
 
 public class DistinctUtility {
     public static List<Integer> getTwoDisjunctNumbers(Random random, int max) {
-        int randomNumber;
-        List<Integer> temporaryList;
-        List<Integer> additionalNumbers;
+        return getNDisjunctIntegers(random, 2, max);
+    }
 
-        randomNumber = random.nextInt(max);
-        temporaryList = new ArrayList<>();
+    public static List<Integer> getNDisjunctIntegers(Random random, int count, int max) {
+        int randomNumber = random.nextInt(max);
+        List<Integer> temporaryList = new ArrayList<>();
         temporaryList.add(randomNumber);
 
-        additionalNumbers = getDisjuctNumbersTo(random, temporaryList, 1, max);
-        temporaryList.addAll(additionalNumbers);
+        for( int counter = 0; counter < count; counter++ ) {
+            List<Integer> additionalNumbers = getDisjuctNumbersTo(random, temporaryList, 1, max);
+            temporaryList.addAll(additionalNumbers);
+        }
+
         return temporaryList;
     }
 
     public static List<Integer> getDisjuctNumbersTo(Random random, List<Integer> numbers, int count, int max) {
-        int counter;
-        List<Integer> disjunctNumbers;
+        List<Integer> disjunctNumbers = new ArrayList<>();
 
-        disjunctNumbers = new ArrayList<>();
-
-        for( counter = 0; counter < count; counter++ )
-        {
-            for(;;)
-            {
-                int randomNumber;
-
-                randomNumber = random.nextInt(max);
-                if( !numbers.contains(randomNumber) && !disjunctNumbers.contains(randomNumber) )
-                {
+        for( int counter = 0; counter < count; counter++ ) {
+            for(;;) {
+                int randomNumber = random.nextInt(max);
+                if( !numbers.contains(randomNumber) && !disjunctNumbers.contains(randomNumber) ) {
                     disjunctNumbers.add(randomNumber);
                     break;
                 }
             }
-
         }
 
         return disjunctNumbers;
