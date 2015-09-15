12e66a534867a883c40ff7dc4f765ccc4c380d62
PtrMan <robertw89@googlemail.com>
2015-09-15 21:53:28 +0200

    Neuroid: * fixed small bug with hashcode and equals of NeuronAdress

--- src/main/java/ptrman/mltoolset/Neuroid/NeuronAdress.java
+++ src/main/java/ptrman/mltoolset/Neuroid/NeuronAdress.java
@@ -17,4 +17,20 @@ public class NeuronAdress {
         this.index = index;
         this.type = type;
     }
+
+    @Override
+    public boolean equals(Object other) {
+        if( !(other instanceof NeuronAdress) ) {
+            return false;
+        }
+
+        NeuronAdress otherNeuronAdress = (NeuronAdress)other;
+
+        return otherNeuronAdress.index == index && otherNeuronAdress.type == type;
+    }
+
+    @Override
+    public int hashCode() {
+        return index + type.ordinal();
+    }
 }
